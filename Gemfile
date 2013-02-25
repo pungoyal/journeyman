6449e9d983c49dbf0989fd0052531e110da08a08
Puneet Goyal <pungoyal@gmail.com>
2013-02-25 19:18:04 +0530

    first skeleton

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+
+gem 'unicorn'
+gem 'mongoid'
+gem 'slim'
+gem 'less'
+
+gem 'twitter-bootstrap-rails'
+
+group :assets do
+  gem 'less-rails'
+  gem 'coffee-rails'
+
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier'
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+end
+
+group :development do
+  gem 'slim-rails'
+  gem 'less-rails'
+end
+
+group :test do
+  gem 'mongoid-rspec'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Journeyman::Application.config.secret_token = 'adf92ae5fa2772120245e94fc015dff2e99f3e4279b17d2d820a4546a84d075c12220463ceefe0ced60a56f792a9f1a4b93c1497244b342f81807d7642ac0dde'
